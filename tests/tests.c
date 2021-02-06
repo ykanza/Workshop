d329c6f34a7c470d7976655b8a2c09859e43ff89
Poisson <antoine.poisson@epitech.eu>
2021-02-06 17:28:14 +0100

    Tu m'as trouvé

---
+++ tests/tests.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** Workshop
+** File description:
+** tests file
+*/
+
+#include <criterion/criterion.h>
+
+#include "tek.h"
+
+Test(FUNCTIONAL_TEST, get_first_sentence)
+{
+    cr_assert_str_not_empty(get_sentence(0));
+}
